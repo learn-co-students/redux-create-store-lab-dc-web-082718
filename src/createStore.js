4517e7cd71e538b7ef03694f05e0859f10c2095c
jb5595 <jacobberman1995@gmail.com>
2018-11-14 16:40:19 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,24 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    if (state){
+      return state;
+    }else{
+      return reducer(state, {type: "init"})
+    }
+  }
+
+  return {
+    dispatch,
+    getState
+  };
 }
 
 function render() {
