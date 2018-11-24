9c2c805467ddaf7634c6e47a98813c673670ea8b
Trevor Jameson <github email address>
2018-11-24 12:31:37 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,23 +1,30 @@
-export default function createStore(reducer) {
-  let state;
+import candyReducer from './reducers/candyReducer'
+import countReducer from './reducers/countReducer'
+
+function createStore(reducer) {
+  let state
+
+  function getState() {
+    return state;
+  }
 
   function dispatch(action) {
     state = reducer(state, action)
     render()
   }
 
-  function getState() {
-    return state;
-  }
+  // @@INIT is the string literal used by convention, but this could be any string that does not have a corresponding type
+  dispatch({ type: '@@init' })
 
-  dispatch({ type: '@@INIT' })
+  return({
+    getState,
+    dispatch
+  })
 
-  return {
-    dispatch,
-    getState
-  }
 }
 
 function render() {
-  const container = document.getElementById('container');
+  const container = document.getElementById('container')
 }
+
+export default createStore
